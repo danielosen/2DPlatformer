ca8bab7343b74c63db7f697141dbb0c9d6137f5c
danielosen <danieleo@student.matnat.uio.no>
2017-07-03 19:01:51 +0200

    stasj

--- scripts/player_test.gd
+++ scripts/player_test.gd
@@ -1,52 +1,50 @@
 extends Node2D
 ### Nodes ###
-onready var tilemap = get_parent().get_node("TileMap")
+onready var tilemap = get_parent().get_node("map/TileMap")
+onready var sprite = get_node("Sprite")
 
 ### Member Variables ###
-const TILE_SLOPE_45_UP= 9
-const TILE_SLOPE_45_DOWN = 10
-const TILE_SIZE = 16
-const TILE_BLOCK = 1
-const WALK_SPEED = 5
-var sprite_extents = Vector2(TILE_SIZE,2*TILE_SIZE) #1-Tile Wide, 2 Tiles Tall
+const WALK_SPEED = 3
+onready var sprite_extents = Vector2(tilemap.TILE_SIZE,2*tilemap.TILE_SIZE) #1-Tile Wide, 2 Tiles Tall
 
 ### Input Functions ###
 
 ### Movement Functions ###
 func handle_horizontal_movement():
 	#The Origin of The Kinematic Character is at the top-left corner of the AABB with sprite_extents
-	var delta_x = 0
+	var dir = 0
 	var input_left = Input.is_action_pressed("left")
 	var input_right = Input.is_action_pressed("right")
-	var forward_position_at_head = get_pos()
+	var space = get_world_2d().get_direct_space_state()
+	var total_translate = Vector2(0,0)
+	var forward_pos = Vector2(get_pos().x+sprite_extents.x/2,get_pos().y+sprite_extents.y+1)
 	if input_left:
-		delta_x += -WALK_SPEED
-		forward_position_at_head.x -= sprite_extents.x
-	if input_right:
-		delta_x += WALK_SPEED + sprite_extents.x #add this to delta_x to not check the tile we are already in
-		forward_position_at_head.x += sprite_extents.x
-	if delta_x != 0:
-		print("current tile pos at head: ",tilemap.world_to_map(get_pos()))
-		var tiles = tilemap.scan_intersecting_tiles(forward_position_at_head,sprite_extents.y,delta_x)
-		if not tiles.empty():
-			handle_horizontal_collision(tiles,delta_x)
-		else:
-			translate(Vector2(sign(delta_x)*WALK_SPEED,0))
+		dir -= 1
+		sprite.set_flip_h(true)
+	if input_right: 
+		dir += 1
+		sprite.set_flip_h(false)
+	if dir != 0:
+		var tile_at_feet
+		for dx in range(WALK_SPEED):
+			tile_at_feet = tilemap.get_cellv(tilemap.world_to_map(forward_pos+total_translate))
+			#var tile_at_feet_forward = tilemap.get_cellv(tilemap.world_to_map(Vector2(get_pos().x+sign(delta_x*sprite_extents.x,get_pos().y+sprite_extents.y)))
+			print(tile_at_feet)
+			if tile_at_feet == tilemap.TILE_BLOCK:
+				total_translate += Vector2(dir,0)
+			elif tile_at_feet == tilemap.TILE_SLOPE45UP:
+				total_translate += Vector2(dir,-dir)
+			elif tile_at_feet == tilemap.TILE_SLOPE30UP:
+				total_translate += Vector2(dir,-0.5*dir)
+			elif tile_at_feet == tilemap.TILE_SLOPE30DOWN:
+				total_translate += Vector2(dir,dir)
+			elif tile_at_feet == tilemap.TILE_SLOPE45DOWN:
+				total_translate += Vector2(dir,dir)
 		
-### Collision Functions ###
-func handle_horizontal_collision(tiles,delta_x):
-	var walls = []
-	for key in tiles.keys():
-		print(key)
-	if tiles.has(TILE_BLOCK):
-		walls = tiles[TILE_BLOCK]
-		walls.sort()
-	if delta_x > 0 and not walls.empty():
-		set_pos(Vector2(walls[0].x-sprite_extents.x,get_pos().y))
-	elif delta_x < 0 and not walls.empty():
-		set_pos(Vector2(walls[-1].x+sprite_extents.x,get_pos().y))
-	pass
+		translate(Vector2(total_translate.x,total_translate.y))
+		print(forward_pos)
 	
+### Collision Functions ###
 ### FIXED PROCESS ###
 func _fixed_process(dt):
 	handle_horizontal_movement()
@@ -54,7 +52,7 @@ func _fixed_process(dt):
 	
 ### DRAWING ###
 func _draw():
-	draw_rect(Rect2(0,0,sprite_extents.x,sprite_extents.y),Color(1,0,0,1))
+	#draw_rect(Rect2(0,0,sprite_extents.x,sprite_extents.y),Color(1,1,0,1))
 	pass
 	
 func _ready():
--- scripts/tilemap.gd
+++ scripts/tilemap.gd
@@ -1,4 +1,10 @@
 extends TileMap
+const TILE_SIZE = 16
+const TILE_BLOCK = 0
+const TILE_SLOPE30UP = 3
+const TILE_SLOPE45UP = 1
+const TILE_SLOPE45DOWN = 2
+const TILE_SLOPE30DOWN = 4
 
 func scan_intersecting_tiles(origin,height,delta_x):
 	#scans downwards, i.e. assuming the origin of the character's AABB is at the top-left corner.
@@ -7,15 +13,18 @@ func scan_intersecting_tiles(origin,height,delta_x):
 	var tile_endpos_upper = world_to_map(origin+Vector2(delta_x,0))
 	var tile_endpos_lower = world_to_map(origin+Vector2(delta_x,height))
 	var steps_x = abs(tile_endpos_lower.x - tile_startpos_lower.x)
-	var steps_y = tile_endpos_lower.y - tile_endpos_upper.y
+	var steps_y = tile_endpos_lower.y - tile_endpos_upper.y+1
+	if tile_endpos_lower.y == 0:
+		steps_y += 1
 	var sign_delta = sign(delta_x)
 	var tiles = {}
+	print(tile_startpos_lower,tile_endpos_lower)
 	print("start scan... ", "steps_x: ",steps_x," steps_y: ",steps_y)
 	for dx in range(steps_x):
 		for dy in range(steps_y):
 			var tile_currentpos = tile_startpos_upper + Vector2(sign_delta*dx,dy)
 			var tile = get_cellv(tile_currentpos)
-			print("tile scanned: ", tile_currentpos)
+			print("tile scanned: ", tile_currentpos, tile)
 			if tile != INVALID_CELL:
 				if not tiles.has(tile):
 					tiles[tile] = []
@@ -25,8 +34,10 @@ func scan_intersecting_tiles(origin,height,delta_x):
 
 func _ready():
 	pass
-	
 func _draw():
+	draw_grid()
+	pass
+func draw_grid():
 	var tilemap_height = get_quadrant_size()
 	var tilemap_width = get_quadrant_size()
 	draw_set_transform(Vector2(-32*8,-32*8), 0, Vector2(16,16))
